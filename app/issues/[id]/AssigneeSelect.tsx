3b2859a4f8bd9fc627db9f191dd846f8566ae644
Aandi1989 <navaliny1989@gmail.com>
2024-02-03 16:59:58 +0300

    Assigning Issues to Users block completed

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,47 +1,63 @@
-'use client';
+"use client";
 
-import { Issue, User } from '@/sql/definitions';
-import { Select } from '@radix-ui/themes'
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Skeleton } from '@/app/components';
+import { Skeleton } from "@/app/components";
+import { Issue, User } from "@/sql/definitions";
+import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-    const {data: users, error, isLoading} = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () => axios.get('/api/users').then(res => res.data),
-        staleTime: 60 * 1000, // 60sec
-        retry: 3
-    });
+  const { data: users, error, isLoading } = useUsers();
 
-    if(isLoading) return <Skeleton />
+  if (isLoading) return <Skeleton />;
 
-    if(error) return null;
+  if (error) return null;
 
+  const assignedIssue = (userId: string) => {
+    axios
+      .patch(
+        "/api/issues/" + issue.id,
+        // we have to send title and description otherwise db will throw error cause these fields cant be null
+        {
+          title: issue.title,
+          description: issue.description,
+          assigninedToUserId: userId || null,
+        }
+      )
+      .catch(() => {
+        toast.error("Changes could not be saved.");
+      });
+  };
 
   return (
-    <Select.Root 
-        defaultValue={issue.assigninedToUserId !== null ? String(issue.assigninedToUserId) : ""}
-        onValueChange={(userId) => {
-        axios.patch('/api/issues/' + issue.id, 
-         // we have to send title and description otherwise db will throw error cause these fields cant be null
-        {title:issue.title, description:issue.description, assigninedToUserId: userId || null 
-        });
-    }}>
+    <>
+      <Select.Root defaultValue={issue.assigninedToUserId !== null ? String(issue.assigninedToUserId): ""}
+                    onValueChange={assignedIssue}>
         {/* @ts-ignore */}
-        <Select.Trigger placeholder='Assign...'/>
+        <Select.Trigger placeholder="Assign..." />
         <Select.Content>
-            <Select.Group>
-                <Select.Label>Suggestions</Select.Label>
-                <Select.Item value="">Unassigned</Select.Item>
-                {users?.map(user => 
-                    <Select.Item key={user.id} value={String(user.id)}>{user.name}</Select.Item>
-                )}
-            </Select.Group>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={String(user.id)}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
         </Select.Content>
-    </Select.Root>
-  )
-}
+      </Select.Root>
+      <Toaster />
+    </>
+  );
+};
 
-export default AssigneeSelect
+const useUsers = () => useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, // 60sec
+    retry: 3,
+  });
+
+export default AssigneeSelect;
